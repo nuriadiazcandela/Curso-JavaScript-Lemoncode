36c7adc3f909833b142ad51588f846de5951690f
nuriadiazcandela <nuriadiazcandela@gmail.com>
2021-09-12 10:24:50 +0200

    Elimino carpeta

--- Modulo-12/src/pages/account/account.api.js
+++
@@ -1,9 +0,0 @@
-import Axios from 'axios';
-
-const url = `${process.env.BASE_API_URL}/account`;
-
-export const insertAccount = account => Axios.post(`${url}/${account.id}`, account).then(({ data }) => data);
-
-export const getAccount = id => Axios.get(`${url}/${id}`).then(({ data }) => data);
-
-export const updateAccount = account => Axios.put(`${url}/${account.id}`, account).then(({ data }) => data);
