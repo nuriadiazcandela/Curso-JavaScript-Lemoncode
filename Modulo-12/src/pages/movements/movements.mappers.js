39942cb325291438364de7158910ad395e73e7aa
nuriadiazcandela <nuriadiazcandela@gmail.com>
2021-09-12 10:11:27 +0200

    fix vulnerabilities

--- Modulo-12/src/pages/movements/movements.mappers.js
+++ Modulo-12/src/pages/movements/movements.mappers.js
@@ -1,22 +1,26 @@
 export const mapMovementsApiToVm = (movements, accountId) =>
-    Array.isArray(movements) ? movements.map(moves => { // recorremos el objeto
-        if (moves.accountId == accountId) { return movementsApiToVm(moves); } //devolvemos los movimientos que coinciden con el id de la cuenta
-    }) : [];
+  Array.isArray(movements)
+    ? movements.map((moves) => {
+        if (moves.accountId == accountId) {
+          return movementsApiToVm(moves);
+        }
+      })
+    : [];
 
-const movementsApiToVm = (moves) => { // obtenemos los datos de cada movimiento
-    return {
-        ...moves,
-        transaction: new Date(moves.transaction).toLocaleDateString(), //formateamos las fechas
-        realTransaction: new Date(moves.realTransaction).toLocaleDateString(),
-        amount: `${moves.amount} €`, //añadimos el símbolo del euro en las cantidades
-        balance: `${moves.balance} €`,
-    };
+const movementsApiToVm = (moves) => {
+  return {
+    ...moves,
+    transaction: new Date(moves.transaction).toLocaleDateString(),
+    realTransaction: new Date(moves.realTransaction).toLocaleDateString(),
+    amount: `${moves.amount} €`,
+    balance: `${moves.balance} €`,
+  };
 };
 
-export const mapEmptyAccountVmToApi = account => {
-    return {
-        ...account,
-        alias: 'Total',
-        iban: 'Todas las cuentas',
-    };
+export const mapEmptyAccountVmToApi = (account) => {
+  return {
+    ...account,
+    alias: 'Total',
+    iban: 'Todas las cuentas',
+  };
 };
